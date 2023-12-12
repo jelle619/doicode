7390be2d62b74f1c4c948b435b38cbe08d139b89
Jelle Zwiers <public@jellezwie.rs>
2023-12-12 12:04:54 +0100

    Nevermind. Make that the cat wise.

--- app/repo/list/page.tsx
+++ app/repo/list/page.tsx
@@ -27,11 +27,12 @@ export default async function Repository() {
   const data = await fetchRepositories();
   return (
     <>
-      <div role="alert" className="alert alert-warning">
+      {data.length >= 100 &&
+      <div role="alert" className="alert alert-warning mb-4">
         <svg xmlns="http://www.w3.org/2000/svg" className="stroke-current shrink-0 h-6 w-6" fill="none" viewBox="0 0 24 24"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M12 9v2m0 4h.01m-6.938 4h13.856c1.54 0 2.502-1.667 1.732-3L13.732 4c-.77-1.333-2.694-1.333-3.464 0L3.34 16c-.77 1.333.192 3 1.732 3z" /></svg>
-        <span>Alleen de meest recente repositories worden hier weergeven. Als jouw gewenste repository niet in de onderstaande lijst staat, push een commit of update een issue en probeer het opnieuw.</span>
+        <span>Alleen de meest recent geüpdatete repositories worden hier weergeven. Als jouw gewenste repository niet in de onderstaande lijst staat, push een commit of update een issue en probeer het opnieuw.</span>
       </div>
-      <br/>
+      }
       <h1>Repositorylijst</h1>
       <List data={data}/>
     </>
