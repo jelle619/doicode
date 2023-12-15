5514c503c17e8fa1838b460df5e618f712733227
Jelle Zwiers <public@jellezwie.rs>
2023-12-15 19:10:34 +0100

    Add Power BI page

---
+++ app/powerbi/page.tsx
@@ -0,0 +1,5 @@
+export default function PowerBI() {
+  return (
+    <iframe className="h-screen w-screen" width="1140" height="541.25" src="https://app.powerbi.com/reportEmbed?reportId=d1d91076-4e35-4640-9163-d49893a9bfe6&autoAuth=true&ctid=3da33584-4a05-42b1-a926-c0bac42de298" allowFullScreen={true}></iframe>
+  )
+}
